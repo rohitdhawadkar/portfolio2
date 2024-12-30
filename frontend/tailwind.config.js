97c3aa519ebee63fd48f5e27bb86e9509898ca8e
rohitdhawadkar <144599518+rohitdhawadkar@users.noreply.github.com>
2024-12-30 18:55:14 +0530

    removed material tailwind components

--- frontend/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-import withMT from "@material-tailwind/react/utils/withMT";
-
-export default withMT({
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-});
